007fe19e11d8ba9c91976d22edc2f52b475ffd47
AhmedCoolProjects <bargadyahmed2000@gmail.com>
2023-08-05 16:09:23 +0100

    done, with not test with the sonic engine template yet

---
+++ main.py
@@ -0,0 +1,54 @@
+import json
+import os
+from time import time
+from yapsy.IMultiprocessPlugin import IMultiprocessPlugin
+from sonic_engine.model.log import Logger
+from sonic_engine.model.extension import ReportingConfig
+from sonic_engine.util.functions import loadConfig, relative
+from sonic_engine.core.database import __db__
+
+
+class HelloWorldReportingExtension(IMultiprocessPlugin):
+    def __init__(self, p):
+        IMultiprocessPlugin.__init__(self, p)
+
+        # TODO: create a simple function that gonna do the same thing, no need to explicitly use loadConfig
+        self.config = loadConfig(ReportingConfig, relative(
+            __file__, './config.yaml'))
+
+        __db__.register_extension(self.config)
+
+        print("--> Hello World Reporting Extension Loaded")
+
+    def run(self):
+
+        logger = Logger(self.config.log, __name__.split('.')[-1])
+
+        i = 0
+        t = time()
+
+        # Open log files
+        bulk = ''
+
+        for data in __db__.get_message():
+            if data['type'] == 'message':
+
+                if time() - t > 3:
+                    logger.debug(
+                        f'{i} items processed from {data["queue_length"]}')
+                    t = time()
+                    if not os.path.exists(relative(__file__, self.config.log.dir)):
+                        os.makedirs(
+                            relative(__file__, self.config.log.dir))
+                    if not os.path.exists(relative(__file__, self.config.log.dir, 'log.txt')):
+                        with open(relative(__file__, self.config.log.dir, 'log.txt'), 'w') as f:
+                            f.write('')
+
+                    with open(relative(__file__, self.config.log.dir, 'log.txt'), 'a') as f:
+                        f.write(bulk)
+                        bulk = ''
+
+                i += 1
+                inference = __db__.retrieve('inference', data['data'])
+                log = json.dumps(inference)
+                bulk += log + '\n'
